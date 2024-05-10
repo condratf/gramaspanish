bd978e86314a366e41101a178b559339f24423f0
f.kondratov <f.kondratov@idynsys.org>
2024-05-10 22:11:48 +0200

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
-
 import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gramaspanish.com',
   output: 'server',
-  integrations: [react(), db()],
+  integrations: [
+    react(),
+    db(),
+  ],
   adapter: netlify(),
   i18n: {
     defaultLocale: "en",
